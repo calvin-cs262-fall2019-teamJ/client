65cc0a18d22c1d7917fba1350ba0999542c0ed17
Samigirma1 <samuel_appsandsubscriptions@yahoo.com>
2019-09-20 22:56:18 -0400

    Hello World App in React Native

---
+++ my-new-cc-client/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ahoy from Team-J! We are using react native!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
