64776ca38a8a7e19c7882a7aa4c0d21726981161
Samigirma1 <46662279+Samigirma1@users.noreply.github.com>
2019-10-11 01:25:55 -0400

    updated news feed

--- components/NewsFeed/NewsFeed.js
+++ components/NewsFeed/NewsFeed.js
@@ -1,23 +1,109 @@
 import React from 'react';
-import { Layout, Text } from 'react-native-ui-kitten';
+import { Button, Icon, List, ListItem, Layout, Text } from 'react-native-ui-kitten';
+import { Image, StyleSheet, ScrollView, SafeAreaView, View, TouchableHighlight } from "react-native";
+import Constants from "expo-constants";
+import { Card } from "react-native-paper";
+
+import PostCard from './PostCard';
 
 class NewsFeed extends React.Component<Props> {
 
  static navigationOptions = ({ navigation }) => ({
-    title: "NewsFeed Page"
+    title: "Calvin Connect",
+    headerLeft: <TouchableHighlight
+      onPress = {() => {alert("Hi! I'm Prof. Vander Linden")}}
+      >
+      <Image
+        style = {{
+          height: 30,
+          width: 30,
+          marginLeft: 10,
+          borderRadius: 15,
+          borderWidth: 2
+        }}
+        source = {require('../../assets/kvlinden1.png')}
+      />
+      </TouchableHighlight>,
+
+      headerRight: 
+      <TouchableHighlight
+      onPress = {() => {alert("You're looking beautifu today!")}}
+      >
+      <Image
+        style = {{
+          height: 30,
+          width: 30,
+          marginRight: 5,
+          borderRadius: 15,
+          borderWidth: 2
+        }}
+        source = {require('../../assets/icons/chatbubble.svg')}
+      />
+      </TouchableHighlight>
   });
 
- 
+profilePress = () => {
+  alert("Going to profile")
+}
   render(){
     return (
-      <Layout>
-        <Text> 
-          Soon to be changed
-        </Text>
-      </Layout>
+      <SafeAreaView style = {styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <PostCard 
+            bgcolor = "white"
+            text = {postText}
+            imageSrc = './quick-brown-fox-18.jpg'
+            userImageSrc = '../../assets/kvlinden.png'
+          />
+          <PostCard 
+            bgcolor = "white"
+            text = {postText}
+            imageSrc = './quick-brown-fox-18.jpg'
+            userImageSrc = '../../assets/kvlinden.png'
+          />
+          <PostCard 
+            bgcolor = "white"
+            text = {postText}
+            imageSrc = './quick-brown-fox-18.jpg'
+            userImageSrc = '../../assets/kvlinden.png'
+          />
+          <PostCard 
+            bgcolor = "white"
+            text = {postText}
+            imageSrc = './quick-brown-fox-18.jpg'
+            userImageSrc = '../../assets/kvlinden.png'
+          />
+        </ScrollView>
+      </SafeAreaView>
     );
   }
-
 }
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 0,
+    backgroundColor: "#EFEFEF"
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  scrollView: {
+    backgroundColor: '#EFEFEF',
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    marginHorizontal: 1,
+    marginTop: 8 
+  },
+  text: {
+    justifyContent: "left",
+  }
+});
+
+const postText = "The quick brown fox jumped over the lazy dog"
 export default NewsFeed;
---
+++ components/NewsFeed/PostCard.js
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image, TouchableHighlight } from 'react-native';
+import { Card } from 'react-native-paper';
+import { Button } from 'react-native-ui-kitten';
+
+/*
+ * @author Samuel Zeleke sgz4@students.calvin.edu
+ * @version 1.0
+ *
+ * AssetExample is a resuable component that Contains a card, a text, 
+ * an image, and a button the inverts the color
+ *
+ */
+export default class PostCard extends React.Component {
+  /*Function accepts props from parent component
+   *and sets the default states
+   *
+   *@param props properties accepted at declaration
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundC: this.props.bgcolor,
+      text: this.props.text,
+      imageSrc: this.props.imageSrc,
+      imageUserPhotoSrc: this.props.userImageSrc,
+      likeTally: 0,
+      buttonTitle: "Like",
+      buttonStatus: "primary"
+    };
+  }
+
+  /*Function inverts the colors of the items in components with-in the
+   *the AssetExample component
+   *
+   *@param
+   */
+  increaseLike = () =>{
+
+    if (this.state.buttonTitle == "Like"){
+      this.setState(
+      {
+        likeTally: this.state.likeTally + 1,
+        buttonTitle: "Liked",
+        buttonStatus: "success"
+      } 
+      )
+      alert("Liked " + this.state.likeTally + "times")
+    }
+    else{
+      this.setState(
+      {
+        likeTally: this.state.likeTally - 1,
+        buttonTitle: "Like",
+        buttonStatus: "primary"
+      } 
+      )
+      alert("Liked " + this.state.likeTally + "times")
+    }
+  }
+
+  /* Renders the component*/
+  render() {
+    return (
+      //Card containing sub-components
+      <Card style = {
+        {
+          marginTop: 8
+        }
+      }>
+        {/*View Containing Text and image*/}
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 24,
+            backgroundColor: "white",
+          }}>
+
+          {/*Text sub-component*/}
+          <Text
+            style={{
+              margin: 24,
+              marginTop: 0,
+              fontSize: 14,
+              textAlign: 'left',
+            }}>
+            {this.props.text}
+          </Text>
+
+          {/*Image sub-component*/}
+          <Image
+            style = {styles.logo}
+            source={require('./quick-brown-fox-18.jpg')}
+            resizeMode = "stretch"
+          />
+        </View>
+
+        {/*Button that inverts the color of the images*/}
+        <Button
+        appearance = "outline" 
+        onPress = {() => this.increaseLike()}
+        status = {this.state.buttonStatus}
+        >
+        {this.state.buttonTitle}
+        </Button>
+      </Card>
+    );
+  }
+}
+
+var style
+const styles = StyleSheet.create({
+  logo: {
+    height: 150,
+    width: 350
+  },
+});
