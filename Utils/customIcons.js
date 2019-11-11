ed268704fc19e4d5d899983124bd8abf2cd6bd5e
ekm6 <47467035+ekm6@users.noreply.github.com>
2019-11-11 18:20:44 -0500

    adds icons and header elements
    still a work in progress to get the props to work

---
+++ Utils/customIcons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+  Icon,
+  Layout,
+  TopNavigation,
+  TopNavigationAction,
+} from 'react-native-ui-kitten';
+
+export const BackIcon = style => <Icon {...style} name="arrow-back" />;
+
+export const MenuIcon = style => <Icon {...style} name="menu-outline" />;
