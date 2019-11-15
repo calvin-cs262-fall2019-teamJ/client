c0641a51c9c248ab8cc0eba9e6e7ec4b40e47251
ekm6 <47467035+ekm6@users.noreply.github.com>
2019-11-15 14:32:04 -0500

    new firebase faire with firestore functionality

--- components/Fire.js
+++ components/Fire.js
@@ -1,23 +1,26 @@
-import firebase from 'firebase'; // 4.8.1
+import { firebase } from '@firebase/app';
+import '@firebase/firestore';
+import '@firebase/auth';
 
 class Fire {
   constructor() {
     if (!firebase.apps.length) {
       this.init();
     }
+    this.dbh = firebase.firestore();
     // this.observeAuth();
   }
 
   init = () =>
     firebase.initializeApp({
-    apiKey: "AIzaSyDOJIsQAZZrzEcOtFJ3QTor6_US2lMcCBw",
-    authDomain: "cs262j.firebaseapp.com",
-    databaseURL: "https://cs262j.firebaseio.com",
-    projectId: "cs262j",
-    storageBucket: "cs262j.appspot.com",
-    messagingSenderId: "96122407891",
-    appId: "1:96122407891:web:85c939306a90d8eae9bd26",
-    measurementId: "G-PY37PP5T20"
+      apiKey: 'AIzaSyDOJIsQAZZrzEcOtFJ3QTor6_US2lMcCBw',
+      authDomain: 'cs262j.firebaseapp.com',
+      databaseURL: 'https://cs262j.firebaseio.com',
+      projectId: 'cs262j',
+      storageBucket: 'cs262j.appspot.com',
+      messagingSenderId: '96122407891',
+      appId: '1:96122407891:web:85c939306a90d8eae9bd26',
+      measurementId: 'G-PY37PP5T20',
     });
 
   observeAuth = () =>
@@ -25,7 +28,7 @@ class Fire {
 
   onAuthStateChanged = user => {
     //firebase.auth().createUserWithEmailAndPassword("akonne16@gmail.com", "thisiscoolNow")
-    alert("loging in")
+    alert('loging in');
     // .catch( (error)=> {
     //   // Handle Errors here.
     //   var errorCode = error.code;
@@ -36,78 +39,56 @@ class Fire {
   };
 
   Login = (email, password) => {
-    return (
-      firebase.auth().signInWithEmailAndPassword(email, password)
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
       .then(function(firebaseUser) {
-          return "success"
+        return 'success';
       })
       .catch(function(error) {
-          return error.message
-      })
-    )
+        return error.message;
+      });
   };
 
- SignUp = (email, password) => {
-   alert("signup")
-   return (
-      firebase.auth().createUserWithEmailAndPassword(email, password)
+  SignUp = (email, password) => {
+    alert('signup');
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
       .then(function(firebaseUser) {
-          return "success"
+        return 'success';
       })
       .catch(function(error) {
-          return error.message
-      })
-    )
+        return error.message;
+      });
   };
 
   get uid() {
     return (firebase.auth().currentUser || {}).uid;
   }
 
-  get ref() {
-    return firebase.database().ref('messages');
-  }
-
-  parse = snapshot => {
-    const { timestamp: numberStamp, text, user } = snapshot.val();
-    const { key: _id } = snapshot;
-    const timestamp = new Date(numberStamp);
-    const message = {
-      _id,
-      timestamp,
-      text,
-      user,
-    };
-    return message;
+  //takes a name and returns the profile, will need to take more parametrs in the future
+  PullProfileInfo = name => {
+     let datalist = [] // where to store the data
+    return this.dbh
+      .collection('profile')
+      .where('name', '==', name)
+      .get()
+      .then(function(querySnapshot) {
+        //necessary if there are multiple results
+        querySnapshot.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          datalist.push(doc.data()); // add the data to the array
+        });
+      })
+      .then(function(data) {
+        return datalist[0] //return the top element
+      })
+      .catch(function(error) {
+        console.log('Error getting documents: ', error);
+        return error;
+      });
   };
-
-  on = callback =>
-    this.ref
-      .limitToLast(20)
-      .on('child_added', snapshot => callback(this.parse(snapshot)));
-
-  get timestamp() {
-    return firebase.database.ServerValue.TIMESTAMP;
-  }
-  // send the message to the Backend
-  send = messages => {
-    for (let i = 0; i < messages.length; i++) {
-      const { text, user } = messages[i];
-      const message = {
-        text,
-        user,
-        timestamp: this.timestamp,
-      };
-      this.append(message);
-    }
-  };
-
-  append = message => this.ref.push(message);
-
-  // close the connection to the Backend
-  off() {
-    this.ref.off();
-  }
 }
 
 Fire.shared = new Fire();
