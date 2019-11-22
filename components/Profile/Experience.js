95e469ae2d08cdadfe60f69d779c496f4bb32a11
Cora-Boyoung-Jung <55420760+Cora-Boyoung-Jung@users.noreply.github.com>
2019-11-22 01:44:44 -0500

    update queried experience

--- components/Profile/Experience.js
+++ components/Profile/Experience.js
@@ -2,32 +2,14 @@ import React from 'react';
 import { Header } from 'react-native-elements';
 import { Layout, Text, ListItem } from 'react-native-ui-kitten';
 import { StyleSheet, View } from 'react-native';
+import Fire from '../Fire';
 
 // defines the format of the "Expirience" section of the profile
 class Experiences extends React.Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      jobs: [
-        {
-          title: 'Professor of Computer Science',
-          company: 'Calvin University',
-          dateRange: '1996 - Present 	•  23 years',
-          location: 'Greater Grand Rapids, Michigan Area',
-        },
-        {
-          title: 'Visiting Researcher',
-          company: 'CSIRO ICT Centre',
-          dateRange: '1997 - 2018  •  21 years',
-          location: 'Sydney, Australia',
-        },
-        {
-          title: 'Research Fellow',
-          company: 'ITRI - University of Brighton',
-          dateRange: '1993 - 1996  	•  3 years',
-          location: 'Brighton, United Kingdom',
-        },
-      ],
+      jobs: [],
     };
   }
 
@@ -35,12 +17,18 @@ class Experiences extends React.Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.inside} category="P1">
-          {this.state.jobs.map(job => {
+          {this.props.jobs.map(job => {
             return (
-              job.title +'\n' +
-              job.company + '\n' +
-              job.dateRange +'\n' +
-              job.location +'\n\n'
+              job.title +
+              '\n' +
+              job.company +
+              '\n' +
+              job.startYear +
+              ' - ' +
+              job.endDate +
+              '\n' +
+              job.location +
+              '\n\n'
             );
           })}
         </Text>
